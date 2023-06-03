4760dfa832c5ddde2d251fb613166a662dd8edf3
sahilborse18 <sahilborse.909098@gmail.com>
2023-06-03 16:27:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,17 +15,28 @@ function changer(){
     'india':"rupees",
     'america':"dollar",
     'europe':"euro",
-    'russia':"ruble"
+    'russia':"ruble",
+    'japan':"yen",
+    'afghanisthan':"afghani",
+    'australian':"dollar",
+    'bahraini':"dinar",
+    'china':"yuan"
   };
   let amt ={
     'india': 82.2818,
     'america':1,
     'europe':0.9195,
     'russia':79.4455,
+    'japan':1.6983,
+    'afghanisthan':87.15,
+    'australian':1.5124,
+    'bahraini':0.377,
+    'china':49.5936,
   };
   var a=amnt.value
-  var b = country.value;
-  var c= otp.value; 
+  var b = country.value.toLowerCase();
+  var c= otp.value.toLowerCase; 
+  console.log(b)
   let outp = a*(amt[c])/(amt[b]);
   console.log(outp);
   let newFile = "The Amount in " + count[c] +" is=" + outp;
